71155ff29d091656a17fa527aff9a0e169546e4c
liubo <13621905107@163.com>
2021-02-26 16:47:44 +0800

    1

---
+++ Test.h
@@ -0,0 +1,16 @@
+//
+//  Test.h
+//  AFNetworking
+//
+//  Created by l on 2021/2/26.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Test : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Test.m
@@ -0,0 +1,12 @@
+//
+//  Test.m
+//  AFNetworking
+//
+//  Created by l on 2021/2/26.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
